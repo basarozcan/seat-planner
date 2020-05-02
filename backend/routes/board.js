f0497365efcdc6ae110414f1dbc2107d7a6d7f49
Unknown <basarozcan@gmail.com>
2020-05-02 19:23:09 +0300

    callback function defined for board route

--- backend/routes/board.js
+++ backend/routes/board.js
@@ -3,15 +3,28 @@ const router = express.Router()
 
 const BoardService = require('../services/board-service')
 
+// TODO - user_id yollayıp boardlarını çekelim
 router.get('/all/json', async (req, res) => {
   const boards = await BoardService.findAll()
   res.send(boards)
 })
 
+router.get('/all/:uid/json', async (req, res) => {
+  console.log(req.params.uid);
+  const boards = await BoardService.findFilter({owner_id: req.params.uid})
+  res.send(boards)
+})
+
 router.get('/:id/json', async (req, res) => {
-  const board = await BoardService.find(req.params.id)
-  if (!board) res.status(404)
-  res.send(board)
+  console.log(req.params.id);
+  await BoardService.find(req.params.id, (err, board) => {
+    if(err){
+      console.log('board not found');
+      res.status(404).send('board not found')
+    }else{
+      res.send(board)
+    }
+  })
 })
 
 router.post('/', async (req, res) => {
