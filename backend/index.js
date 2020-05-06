ccb3e0210d8164e001b3316645e86eaf32161f33
Unknown <basarozcan@gmail.com>
2020-05-06 17:26:14 +0300

    adding auth middleware
    used for trial

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,7 @@ const app = require('./app')
 const port = process.env.PORT || 3000
 
 app.listen(port, () => {
-  console.log(`Please visit http://localhost:${port}`)
+  console.log(`Please visit: http://localhost:${port}`)
   // console.log('PORT',process.env.PORT);
   // console.log('MONGODB_CONNECTION_STRING', process.env.MONGODB_CONNECTION_STRING);
 })
