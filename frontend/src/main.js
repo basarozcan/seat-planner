29daf5cc3fb3028bfde1b02f1dc4c00e4fe251d1
basarozcan <basarozcan@gmail.com>
2020-05-18 01:35:59 +0300

    tailwindcss added as npm package

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import App from "./App.vue";
 import store from './store'
+import "./assets/css/tailwind.css";
 import VueRouter from 'vue-router'
 import { routes } from './routes'
 
